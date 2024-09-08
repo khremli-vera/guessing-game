55e633988dc1ac99d5ae449b47adcd898194b284
Vera <lovet.post@gmail.com>
2024-09-08 10:47:03 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.isFirstTime = true;
+    }
 
     setRange(min, max) {
-
+        if (this.isFirstTime) {
+            this.min = min;
+            this.max = max;
+        }
     }
 
     guess() {
-
+        return Math.round(this.min + (this.max - this.min) / 2)
     }
 
     lower() {
-
+        this.max = Math.round(this.min + (this.max - this.min) / 2)
     }
 
     greater() {
-
+        this.min = Math.round(this.min + (this.max - this.min) / 2)
     }
 }
 
